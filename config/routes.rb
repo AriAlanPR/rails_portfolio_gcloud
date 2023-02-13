72d7d9f0dd1f8a1fbfe8b1f8fc5df1ca70512285
AriAlanPR <shaymin2003@gmail.com>
2023-02-13 05:51:33 +0000

    initial setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
 end
